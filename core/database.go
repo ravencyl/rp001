773f52c75f2e6c7bb3662bbde8f75c1a3b3988d1
ravencyl <ravencyl@gmail.com>
2022-08-15 16:43:14 +0800

    sqlite support

---
+++ core/database.go
@@ -0,0 +1,27 @@
+package core
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// https://gorm.io/docs/
+
+func DBOpen() *gorm.DB {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	db, err := gorm.Open(sqlite.Open(os.Getenv("DATABASE_FILE")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
