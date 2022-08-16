7b64bd19a3c9a43885df4c02170670f6126065d5
ravencyl <ravencyl@gmail.com>
2022-08-16 16:14:02 +0800

    build admin template sys

---
+++ views/admin_view.go
@@ -0,0 +1,33 @@
+package views
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func AdminHandler(w http.ResponseWriter, r *http.Request) {
+
+	DatabaseMigration(w)
+	// param := strings.TrimPrefix(r.URL.Path, "/admin/")
+	// fmt.Println(param)
+
+	// if param == "" {
+	// fmt.Println("empty param, go default page")
+	// }
+}
+
+func DatabaseMigration(w http.ResponseWriter) {
+	tpl, err := template.ParseFiles(
+		"./templates/admin/page_admin_base.html",
+		"./templates/admin/block_slidebar.html",
+		"./templates/admin/block_header.html",
+		"./templates/admin/block_system.html",
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tpl.Execute(w, nil)
+}
